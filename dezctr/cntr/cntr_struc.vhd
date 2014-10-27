850894dc4768b05a19de3409c8719446eec1b482
David Wolf <d.wolf@live.at>
2014-10-27 16:45:36 +0100

    reset hinzugefügt

--- dezctr/cntr/cntr_struc.vhd
+++ dezctr/cntr/cntr_struc.vhd
@@ -16,41 +16,53 @@ use IEEE.numeric_std.all;
 --cntr3_o   : out std_logic_vector(3 downto 0));
 
 architecture rtl of cntr is
+    -- Es gibt vier ZustÃ¤nde
+    -- UP:    Zählt rauf
+    -- DOWN:  Zählt runter
+    -- HOLD:  Macht nichts
+    -- RESET: Setzt alle Ziffern auf 0
     type t_state is (UP, DOWN, HOLD, RESET);
-    signal s_present_state : t_state;
-    signal s_next_state    : t_state;
+    signal s_present_state : t_state; -- Der jetzige Zustand
+    signal s_next_state    : t_state; -- Der nächste Zustand
     
-    signal s_cntr0_o : std_logic_vector(3 downto 0);
-    signal s_cntr1_o : std_logic_vector(3 downto 0);
-    signal s_cntr2_o : std_logic_vector(3 downto 0);
-    signal s_cntr3_o : std_logic_vector(3 downto 0);
+    signal s_cntr0_o : std_logic_vector(3 downto 0); -- Interner Zähler auf Ziffer 1
+    signal s_cntr1_o : std_logic_vector(3 downto 0); -- Interner Zähler auf Ziffer 2
+    signal s_cntr2_o : std_logic_vector(3 downto 0); -- Interner Zähler auf Ziffer 3
+    signal s_cntr3_o : std_logic_vector(3 downto 0); -- Interner Zähler auf Ziffer 4
 begin
     p_state : process(clk50, reset_n)
     begin
-        if (reset_n = '1') then
+        if (reset_n = '1') then -- Externer Reset
             s_present_state <= UP;
             cntr0_o <= "0000";
             cntr1_o <= "0000";
             cntr2_o <= "0000";
             cntr3_o <= "0000";
-        elsif rising_edge(clk50) then
-            s_present_state <= s_next_state;
-            cntr0_o <= s_cntr0_o;
-            cntr1_o <= s_cntr1_o;
-            cntr2_o <= s_cntr2_o;
-            cntr3_o <= s_cntr3_o;
+        elsif rising_edge(clk50) then -- Taktsignal
+            s_present_state <= s_next_state; -- Aktualisiert den Zustand
+            cntr0_o <= s_cntr0_o; -- Setzte externen cntr0
+            cntr1_o <= s_cntr1_o; -- Setzte externen cntr1
+            cntr2_o <= s_cntr2_o; -- Setzte externen cntr2
+            cntr3_o <= s_cntr3_o; -- Setzte externen cntr3
         end if;
     end p_state;
     
     p_count : process(
         clk50,
+        reset_n,
         ctup_i,
         ctdown_i,
         ctreset_i,
         cthold_i,
         s_present_state)
     begin
-        if rising_edge(clk50) then
+        if (reset_n = '1') then
+            s_next_state <= UP;
+            s_cntr0_o <= "0000";
+            s_cntr1_o <= "0000";
+            s_cntr2_o <= "0000";
+            s_cntr3_o <= "0000";
+        elsif rising_edge(clk50) then
             case s_present_state is
                 when UP =>
                     if (ctdown_i = '1') then
